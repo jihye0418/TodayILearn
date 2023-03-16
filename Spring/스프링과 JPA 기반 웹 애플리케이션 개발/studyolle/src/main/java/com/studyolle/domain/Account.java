64d7bd2476a4b56d19cdd164f56efdc55fe0da93
kjh <lykim0418@naver.com>
2023-03-17 00:08:21 +0900

    #1 Account Entity 추가

---
+++ Spring/스프링과 JPA 기반 웹 애플리케이션 개발/studyolle/src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,60 @@
+package com.studyolle.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    //이메일 인증완료 여부
+    private boolean emailVerified;
+
+    //이메일 검증 시 사용할 Token 값
+    private String emailCheckToken;
+
+    //이메일 인증 완료 후 가입 완료
+    private LocalDateTime joinedAt;
+
+    //자기소개
+    private String bio;
+
+    private String url;
+
+    //직업
+    private String occupation;
+
+    //사는 곳
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+}
