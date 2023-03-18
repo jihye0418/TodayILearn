5863956f4898aa1f1f3fd731c97e0148b040cb2c
kjh <lykim0418@naver.com>
2023-03-18 17:28:08 +0900

    [추가] AccountEntity, 회원가입 컨트롤러 임시로 index와 회원가입 화면을 넣었다.

---
+++ Spring/스프링과 JPA 기반 웹 애플리케이션 개발/studyolle/src/main/java/com/studyolle/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.studyolle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //특정한 요청들은 권한 체크를 하지 않도록 설정
+        http.authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up","/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "/login-link").permitAll()
+                ///profile/*은 GET일 경우에만 권한 체크하지 않도록 함.
+                .mvcMatchers(GET,"/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+}
