f84bddcbad32ae142e5b58200bb90c740461351d
kjh <lykim0418@naver.com>
2023-03-18 23:46:21 +0900

    #2 회원가입 서브밋 과정까지만 나타내기
    회원가입 폼에서 넘어오는 값을 검증하는 단계까지만 우선 구현.

---
+++ Spring/스프링과 JPA 기반 웹 애플리케이션 개발/studyolle/src/main/java/com/studyolle/account/AccountRepository.java
@@ -0,0 +1,19 @@
+package com.studyolle.account;
+
+import com.studyolle.domain.Account;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    Account findByEmail(String email);
+
+    Account findByNickname(String nickname);
+}
